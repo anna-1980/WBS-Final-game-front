23cc16b90d03151ac7de43159187ad2ce7b31647
Anna Zdyb <anna.zdyb@gmail.com>
2022-01-19 14:17:12 +0100

    uploading games doesn't work again

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,10 +20,11 @@ const Home = () => {
             <p>Viruses - a tiny things carrying their own genetic material.</p> 
             <p>Genetic material ia a substance that has an information of of how you look and how to make more of you.  </p>
             <p>Curious thing about Viruses is that till now, we are still not sure if they are already alive or not quite yet. It is because of their super simple build.</p>
-           <p>However Viruses need a way to make more of themselves, and for that they need a HOST</p>
-           <p>HOST - is another, more complex organism, Bacterium or a Human that Virus can use to reproduce</p>
-           <p>For that purpose Virus has to infect/enter a cell, then it multiplies inside and leaves, that part is often quite bad fir the HOST </p>
-          <p>Therefore it is better to avoid getting infected by a Virus</p>
+           <p>However Viruses need a way to make more of themselves, and for that they need a HOST.</p>
+           <p>HOST - is another, more complex organism, Bacterium or a Human that Virus can use to reproduce.</p>
+           <p>For that purpose Virus has to infect/enter a cell, then it multiplies inside and leaves, that part is often quite bad fir the HOST. </p>
+          <p>Therefore it is better to avoid getting infected by a Virus, even though our immune system gan take care of most of them.</p>
+          
           </div>
            <Video2 />
         </div>
