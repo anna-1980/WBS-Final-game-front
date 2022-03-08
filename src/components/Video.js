69f1bd335db07b03a0ecdd2986861b7e741537ee
Anna Zdyb <anna.zdyb@gmail.com>
2022-03-08 09:55:39 +0100

    fixing video problem adding codecs attribute

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,10 +6,12 @@ const Video = () => {
  
      <div className="videoCover"></div>
      <div className="videoCover2"></div>
-     <video src={video}  className="video" type="video/mp4" controls></video>
+     <video src={video}  className="video" type='video/mp4; codecs="AAC, H.264"'  controls></video>
 
  </div>
   );
 };
 
 export default Video;
+
+<video ></video>
